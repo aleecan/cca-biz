95c18131adecb219872b095729a603ddc09257dd
philippe <warnonphilippe@gmail.com>
2018-09-03 13:13:22 +0200

    creation de store by code

--- src/main/java/be/civadis/biz/messaging/ArticleConsumerService.java
+++ src/main/java/be/civadis/biz/messaging/ArticleConsumerService.java
@@ -21,8 +21,7 @@ public class ArticleConsumerService {
      */
     @StreamListener(ArticleChannel.INPUT_CHANNEL)
     public void consume(ArticleDTO article) {
-        log.info("Article reçu: {}.", article.getCode());
-        log.info("Update article si présent en DB bonCommande");
+        log.info("Article reçu: {}, Update article si présent en DB bonCommande", article.getCode());
         //TODO : appel service ou repo pour maj de l'article si nécessaire
     }
 
