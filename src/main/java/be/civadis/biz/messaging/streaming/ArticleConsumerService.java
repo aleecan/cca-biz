6ef6eecd1933c8cc6d875046150ff55da4553279
Philippe Warnon <phw@civadis.be>
2018-09-05 11:49:41 +0200

    origin message

--- src/main/java/be/civadis/biz/messaging/streaming/ArticleConsumerService.java
+++ src/main/java/be/civadis/biz/messaging/streaming/ArticleConsumerService.java
@@ -4,6 +4,8 @@ import be.civadis.biz.messaging.dto.ArticleDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,9 +20,9 @@ public class ArticleConsumerService {
      * @param article
      */
     @StreamListener(ArticleChannel.INPUT_CHANNEL)
-    public void consume(ArticleDTO article) {
-        log.info("Article reçu: {}, Update article si présent en DB bonCommande", article.getCode());
-        //TODO: identifier le tenant de l'article
+    public void consume(@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        ArticleDTO article) {
+        log.info("Article reçu: {} from topic {}, Update article si présent en DB bonCommande", article.getCode(), topic);
         //TODO : appel service ou repo pour maj de l'article si nécessaire
     }
 
