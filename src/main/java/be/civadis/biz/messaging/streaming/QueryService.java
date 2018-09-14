7b9d79c9b170df1820d3126dd845a88032fb2d10
philippe <warnonphilippe@gmail.com>
2018-09-14 15:30:19 +0200

    correction query store

--- src/main/java/be/civadis/biz/messaging/streaming/QueryService.java
+++ src/main/java/be/civadis/biz/messaging/streaming/QueryService.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 
 public class QueryService {
 
-    @Autowired
-    protected QueryableStoreRegistry queryableStoreRegistry;
+    //@Autowired
+    //protected QueryableStoreRegistry queryableStoreRegistry;
 
     @Autowired
     private ApplicationProperties applicationProperties;
@@ -23,11 +23,23 @@ public class QueryService {
         this.objectMapper = new ObjectMapper();
     }
 
+    /**
+     * recherche un store dans le kafkaStreams créé par spring cloud stream
+     * Attention, différent de celui créé par StoreService,
+     *   les stores de l'un ne sont pas visibles dans l'autre
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    /*
     protected ReadOnlyKeyValueStore<String, String> getStore(String storeName){
         return queryableStoreRegistry.getQueryableStoreType(
                 storeName,  //TODO: Comment filtrer par tenant ???
                 QueryableStoreTypes.keyValueStore());
     }
+    */
 
     protected <T> T convert(String json, Class<T> clazz) throws IOException {
         return objectMapper.readValue(json, clazz);
